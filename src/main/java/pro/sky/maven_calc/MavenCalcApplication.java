781f22bf46d4b2515759d0e8dbce81dc828ed226
Boroda9992 <fedko.ak@gmail.com>
2024-09-26 20:32:56 +0300

    init commit

---
+++ src/main/java/pro/sky/maven_calc/MavenCalcApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.maven_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/maven_calc/MavenCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.maven_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
